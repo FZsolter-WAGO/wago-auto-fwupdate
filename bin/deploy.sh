742341d5536c690c31c4d3553c8dcb06a284f8a9
FZsolter-WAGO <zsolt.fekete@wago.com>
2023-10-25 15:21:23 +0200

    First test

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+if [[-z "$1"]]; then
+    $1 = "help"
+fi
+if [["$1" = "help"]]; then
+    echo "   wago-auto-fwupdate 1.0.0 - Tool for downloading and deploying a specific firmware version from the Github repository"
+    echo ""
+    echo "   Usage: .../deploy.sh help|FIRMWARE_REVISION"
+    echo ""
+    echo "   Example: .../deploy.sh 04.03.03(25)"
+    exit 0
+fi
 if [ "$EUID" -ne 0 ]
 then
     echo "Please run the script as root"
@@ -6,7 +17,6 @@ then
 fi
 # Lets download the firmware first
 CURRENT_ORDER_NUMBER="$(/etc/config-tools/get_coupler_details order-number)"
-CURRENT_FW_REVISION="$(/etc/config-tools/get_coupler_details firmware-revision)"
 case $CURRENT_ORDER_NUMBER in
     750-8212)
         FW_TYPE=pfc_g2
@@ -17,23 +27,25 @@ case $CURRENT_ORDER_NUMBER in
         ;;
 esac
 echo "Trying to download $CURRENT_FW_REVISION for $CURRENT_ORDER_NUMBER..."
-wget -qO /tmp/FW.md5 --no-check-certificate "https://raw.githubusercontent.com/FZsolter-WAGO/wago-auto-fwupdate/main/firmwares/$FW_TYPE/$CURRENT_FW_REVISION/FW.md5" &>/dev/null
+wget -qO /tmp/FW.md5 --no-check-certificate "https://raw.githubusercontent.com/FZsolter-WAGO/wago-auto-fwupdate/main/firmwares/$FW_TYPE/$1/FW.md5" &>/dev/null
 if [ ! -s "/tmp/FW.md5" ]; then
     echo "Error while downloading MD5 file"
     exit 1
 fi
-wget -qO /tmp/FW.raucb --no-check-certificate "https://raw.githubusercontent.com/FZsolter-WAGO/wago-auto-fwupdate/main/firmwares/$FW_TYPE/$CURRENT_FW_REVISION/FW.raucb" &>/dev/null
+wget -qO /tmp/FW.raucb --no-check-certificate "https://raw.githubusercontent.com/FZsolter-WAGO/wago-auto-fwupdate/main/firmwares/$FW_TYPE/$1/FW.raucb" &>/dev/null
 if [ ! -s "/tmp/FW.raucb" ]; then
-    echo "Error while downloading RAUCB file"
+    echo "Error while downloading firmware file"
     exit 1
 fi
 downloaded_md5=$(md5sum /tmp/FW.raucb | awk '{print $1}')
 expected_md5=$(cat /tmp/FW.md5)
 if [ "$downloaded_md5" = "$expected_md5" ]; then
-    echo "RAUCB file validation successful."
+    echo "Firmware file validation successful."
 else
-    echo "RAUCB file validation failed. The downloaded file is not valid."
+    echo "Firmware file validation failed. The downloaded file is not valid."
     exit 1
 fi
 wget -qO /etc/rc.d/S15_z_auto_fwupdate --no-check-certificate "https://raw.githubusercontent.com/FZsolter-WAGO/wago-auto-fwupdate/main/service/S15_z_auto_fwupdate" &>/dev/null
 chmod +x /etc/rc.d/S15_z_auto_fwupdate &>/dev/null
+echo "Starting the background process"
+/etc/rc.d/S15_z_auto_fwupdate start
